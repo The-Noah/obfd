60b4f55819a0afe9c8fcdaf41a75f7815e908614
The Noah <thenoahbz@gmail.com>
2024-08-22 12:37:56 -0500

    feat: use tokio

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,48 @@
 use std::{
-  fs,
+  future::Future,
   path::{Path, PathBuf},
-  sync::mpsc,
+  pin::Pin,
 };
 
-mod thread_pool;
-use thread_pool::*;
+use tokio::sync::mpsc;
 
-fn main() {
-  let (tx, rx) = mpsc::channel();
+#[tokio::main]
+async fn main() {
+  let path = std::env::args().nth(1).unwrap_or(".".to_string());
+  let path = path.as_str();
+  let path = Path::new(path);
 
-  walk_dir(Path::new("."), tx);
+  if !path.exists() {
+    eprintln!("Path does not exist");
+    return;
+  }
 
-  let thread_pool = ThreadPool::default();
-
-  while let Ok(path) = rx.recv() {
-    thread_pool.execute(move || {
-      move_file(path);
-    });
+  if path.is_file() {
+    move_file(path.to_path_buf(), path.parent().unwrap().to_path_buf()).await;
+  } else {
+    run_on_dir(path).await;
   }
 }
 
-fn move_file(path: PathBuf) {
+async fn run_on_dir(path: &Path) {
+  let (tx, mut rx) = mpsc::channel(100);
+
+  walk_dir(path, tx).await;
+
+  let mut tasks = vec![];
+
+  while let Some(file_path) = rx.recv().await {
+    let path = path.to_path_buf();
+
+    tasks.push(tokio::spawn(async move {
+      move_file(file_path, path).await;
+    }));
+  }
+
+  futures::future::join_all(tasks).await;
+}
+
+async fn move_file(path: PathBuf, working_dir: PathBuf) {
   let filename = path.file_name().unwrap().to_string_lossy();
 
   println!("Moving file: {}", &filename);
@@ -47,26 +68,31 @@ fn move_file(path: PathBuf) {
   let year = date.format("%Y").to_string();
   let month = date.format("%m").to_string();
 
-  let directory = Path::new(".").join(year).join(month);
+  let directory = working_dir.join(year).join(month);
 
   if !directory.exists() {
-    fs::create_dir_all(&directory).expect("Error creating directory");
+    tokio::fs::create_dir_all(&directory).await.expect("Error creating directory");
   }
 
   let new_path = directory.join(filename.to_string());
 
-  fs::rename(&path, &new_path).expect("Error moving file");
+  tokio::fs::rename(&path, &new_path).await.expect("Error moving file");
 }
 
-fn walk_dir(path: &Path, tx: mpsc::Sender<PathBuf>) {
-  if path.is_dir() {
-    for entry in fs::read_dir(path).unwrap() {
-      let entry = entry.unwrap();
-      let path = entry.path();
+fn walk_dir(path: &Path, tx: mpsc::Sender<PathBuf>) -> Pin<Box<dyn Future<Output = ()> + Send + '_>> {
+  Box::pin(async move {
+    if path.is_dir() {
+      let entries = tokio::fs::read_dir(path).await.unwrap();
+      tokio::pin!(entries);
 
-      walk_dir(&path, tx.clone());
+      while let Some(entry) = entries.next_entry().await.unwrap() {
+        let path = entry.path();
+        if path.is_dir() {
+          walk_dir(&path, tx.clone()).await;
+        } else {
+          tx.send(path).await.unwrap();
+        }
+      }
     }
-  } else if path.is_file() {
-    tx.send(path.to_path_buf()).unwrap();
-  }
+  })
 }
